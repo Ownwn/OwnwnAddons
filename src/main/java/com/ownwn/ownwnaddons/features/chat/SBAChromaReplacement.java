5e2b60584c25d6ded953d611d4fd40ab279ae304
Owen <64725743+Ownwn@users.noreply.github.com>
2023-01-01 21:05:18 +1300

    more chroma stuff

--- src/main/java/com/ownwn/ownwnaddons/features/chat/SBAChromaReplacement.java
+++ src/main/java/com/ownwn/ownwnaddons/features/chat/SBAChromaReplacement.java
@@ -25,7 +25,16 @@ public class SBAChromaReplacement {
             newMsg = msg.replace("§aPrototype Lobby", "§zPrototype Lobby");
         } else if (msg.contains("§r§eWelcome to §r§aHypixel SkyBlock§r§e!§r")) {
             newMsg = msg.replace("§aHypixel SkyBlock", "§zHypixel SkyBlock");
+        } else if (msg.contains("§6§lALLOWANCE! §r§eYou earned")) {
+            newMsg = msg.replace("§6§lALLOWANCE!", "§z§lALLOWANCE!");
+        } else if (msg.contains("§d§lYum! §r§eYou gain ")) {
+            newMsg = msg.replace("§d§lYum", "§z§lYum");
+        } else if (msg.contains("§dParty Finder §r§f> §r§aYour party has been queued in the dungeon finder!§r")) {
+            newMsg = msg.replace("§dParty Finder", "§zParty Finder");
+        } else if (msg.contains("§r§5§lEXTRA! §r§dYou got §r§a§lDOUBLE §r§dsalvaged essence!§r§7 (2)§r")) {
+            newMsg = msg.replace("§5§lEXTRA", "§z§lEXTRA");
         }
+
 
 
         if (newMsg.equals("")) {
