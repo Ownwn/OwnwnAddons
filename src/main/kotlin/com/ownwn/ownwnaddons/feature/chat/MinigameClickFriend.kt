165b9b918fc0586129fe311194737aba84af478c
Ownwn <64725743+Ownwn@users.noreply.github.com>
2024-10-21 22:47:56 +1300

    port MinigameClickFriend to kotlin

---
+++ src/main/kotlin/com/ownwn/ownwnaddons/feature/chat/MinigameClickFriend.kt
@@ -0,0 +1,91 @@
+package com.ownwn.ownwnaddons.feature.chat
+
+import cc.polyfrost.oneconfig.events.event.ChatReceiveEvent
+import cc.polyfrost.oneconfig.events.event.ChatSendEvent
+import cc.polyfrost.oneconfig.libs.eventbus.Subscribe
+import cc.polyfrost.oneconfig.libs.universal.UChat
+import com.ownwn.ownwnaddons.Config
+import com.ownwn.ownwnaddons.OwnwnAddons
+import com.ownwn.ownwnaddons.util.Game
+import com.ownwn.ownwnaddons.util.Utils
+import net.minecraft.event.ClickEvent
+import net.minecraft.event.HoverEvent
+import net.minecraft.util.ChatComponentText
+
+
+
+object MinigameClickFriend {
+    private var onCooldown: Boolean = false
+
+    // The list of people you've friended resets when you restart your game, but that's fine IMO
+    private var requestList: MutableList<String> = ArrayList()
+
+    private val joinedRegex = Regex("^(\\w{1,16}) has joined \\(\\d+/\\d+\\)!")
+
+
+
+
+    @Subscribe
+    fun onChat(event: ChatReceiveEvent) {
+        if (!Config.minigameClickFriend) {
+            return
+        }
+
+        val msg: String = Utils.stripFormatting(event.message.formattedText)
+
+        when {
+            msg == "You can only have 10 active requests simultaneously!" -> onCooldown = true
+            msg.startsWith("Your friend request to") && msg.endsWith("has expired.") -> onCooldown = false
+            msg.startsWith("You are now friends with ") -> onCooldown = false
+        }
+
+        if (!msg.endsWith(")!") || msg.contains(Game.name!!)) {
+            return;
+        }
+
+        val match = joinedRegex.find(msg) ?: run {
+            return
+        }
+
+        val joinedName = match.groupValues[1]
+        if (joinedName in requestList) {
+            return
+        }
+
+        event.message.setChatStyle(
+            event.message.chatStyle
+                .setChatClickEvent(ClickEvent(ClickEvent.Action.RUN_COMMAND, "/f add $joinedName"))
+                .setChatHoverEvent(
+                    HoverEvent(
+                        HoverEvent.Action.SHOW_TEXT,
+                        ChatComponentText("§r§aClick to add §b$joinedName §aas a friend!§r")
+                    )
+                )
+        )
+    }
+
+
+    @Subscribe
+    fun onSpeak(event: ChatSendEvent) {
+        if (!Config.minigameClickFriend) {
+            return
+        }
+
+        if (!event.message.startsWith("/f add ")) {
+            return
+        }
+
+        if (onCooldown) {
+            event.isCancelled = true
+            UChat.chat(OwnwnAddons.PREFIX + "&cYou have reached the maximum of 10 requests!")
+            return
+        }
+
+        val playerName = event.message.replace("/f add ", "")
+        if (requestList.contains(playerName)) {
+            event.isCancelled = true
+            UChat.chat(OwnwnAddons.PREFIX + "&cYou have already friended that player!")
+        }
+        requestList.add(playerName)
+    }
+}
