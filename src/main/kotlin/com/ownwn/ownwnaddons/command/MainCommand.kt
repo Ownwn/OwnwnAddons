51ea07fa5593b5b9629bee4a9aa5af5164faf227
Ownwn <64725743+Ownwn@users.noreply.github.com>
2024-10-21 22:47:47 +1300

    refactor main command to kotlin

---
+++ src/main/kotlin/com/ownwn/ownwnaddons/command/MainCommand.kt
@@ -0,0 +1,45 @@
+package com.ownwn.ownwnaddons.command
+
+import cc.polyfrost.oneconfig.libs.universal.UChat
+import cc.polyfrost.oneconfig.utils.Multithreading
+import cc.polyfrost.oneconfig.utils.commands.annotations.*
+import com.ownwn.ownwnaddons.Config
+import com.ownwn.ownwnaddons.OwnwnAddons
+import com.ownwn.ownwnaddons.feature.CustomName
+import com.ownwn.ownwnaddons.feature.dungeons.EnderPearlGrabber
+import com.ownwn.ownwnaddons.util.Game
+import net.minecraft.client.Minecraft
+import net.minecraft.client.audio.ISound
+import net.minecraft.client.audio.PositionedSound
+import net.minecraft.util.ResourceLocation
+import java.util.concurrent.TimeUnit
+
+
+@Command(value = OwnwnAddons.MODID, description = "Access the " + OwnwnAddons.NAME + " GUI.", aliases = ["owa"])
+object MainCommand {
+    @Main
+    fun main() {
+        Config.openGui()
+    }
+
+    @SubCommand(description = "Displays a customizable, formatted chat message in your chat. \"&\"s will be replaced with formatting codes.")
+    private fun preview(@Description("message") @Greedy message: String) {
+        if (message.isEmpty()) {
+            UChat.chat(OwnwnAddons.PREFIX + "&cPlease enter a message to preview!")
+            return
+        }
+        UChat.chat(message.replace("&", "§"))
+    }
+
+    @SubCommand(description = "Refreshes custom names without rejoining")
+    private fun fetchnames() {
+        UChat.chat(OwnwnAddons.PREFIX + "&aRefreshing names...")
+        CustomName.lastFetchedNames = System.currentTimeMillis()
+        Multithreading.schedule(CustomName.fetchOtherNames, 0, TimeUnit.SECONDS)
+    }
+
+    @SubCommand(description = "Grabs ender pearls from your sacks to keep 16 in your inventory")
+    private fun pearls() {
+        EnderPearlGrabber.grabPearls()
+    }
+}
