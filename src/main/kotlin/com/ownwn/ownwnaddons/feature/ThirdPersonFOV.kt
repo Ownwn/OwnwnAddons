aa352909387b42ec2a77f272d6e8f3862388534f
Ownwn <64725743+Ownwn@users.noreply.github.com>
2024-10-21 22:45:40 +1300

    port ThirdPersonFOV to kotlin

---
+++ src/main/kotlin/com/ownwn/ownwnaddons/feature/ThirdPersonFOV.kt
@@ -0,0 +1,45 @@
+package com.ownwn.ownwnaddons.feature
+
+import cc.polyfrost.oneconfig.events.event.TickEvent
+import cc.polyfrost.oneconfig.libs.eventbus.Subscribe
+import com.ownwn.ownwnaddons.Config
+import com.ownwn.ownwnaddons.util.Game
+import com.ownwn.ownwnaddons.util.ServerJoinEvent
+import net.minecraft.client.Minecraft
+
+object ThirdPersonFOV {
+    private var normalFov: Float = 0f
+    private var isThirdPerson = false
+
+    @Subscribe
+    fun onTick(event: TickEvent) {
+        if (!Config.thirdPersonFOV) {
+            return
+        }
+
+        val mc = Game.mc!!
+
+
+        val justChangedPerspective = mc.gameSettings.thirdPersonView == 1 != isThirdPerson
+        if (!justChangedPerspective) {
+            return
+        }
+
+        if (!isThirdPerson) {
+            normalFov = mc.gameSettings.fovSetting // save player's FOV
+            mc.gameSettings.fovSetting = Config.fovModifier
+            isThirdPerson = true
+
+        } else {
+            mc.gameSettings.fovSetting = normalFov // reset FOV to normal
+            isThirdPerson = false
+        }
+
+
+    }
+
+    @Subscribe
+    fun onServerJoin(event: ServerJoinEvent) {
+        normalFov = Minecraft.getMinecraft().gameSettings.fovSetting
+    }
+}
