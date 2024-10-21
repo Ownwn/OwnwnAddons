80e2036ab870d291fd48aa7d2ce8086582287184
Ownwn <64725743+Ownwn@users.noreply.github.com>
2024-10-21 22:47:12 +1300

    feat: ender pearl grabber from sacks

---
+++ src/main/kotlin/com/ownwn/ownwnaddons/feature/dungeons/EnderPearlGrabber.kt
@@ -0,0 +1,25 @@
+package com.ownwn.ownwnaddons.feature.dungeons
+
+import cc.polyfrost.oneconfig.libs.universal.UChat
+import net.minecraft.client.Minecraft
+import net.minecraft.init.Items
+
+object EnderPearlGrabber {
+    fun grabPearls() {
+        val player = Minecraft.getMinecraft().thePlayer ?: return
+
+        var pearlsCount = 0
+        val pearlNum = 16
+
+        for (itemStack in player.inventory.mainInventory) {
+            if (itemStack == null) continue
+
+            if (itemStack.item != Items.ender_pearl) continue
+            pearlsCount += itemStack.stackSize
+        }
+
+        if (pearlsCount >= pearlNum) return
+
+        UChat.say("/gfs ender_pearl ${pearlNum - pearlsCount}")
+    }
+}
