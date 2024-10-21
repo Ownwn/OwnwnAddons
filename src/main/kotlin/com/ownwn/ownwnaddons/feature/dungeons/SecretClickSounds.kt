8a723e8194639904cdce4c91c146d6203a71177b
Ownwn <64725743+Ownwn@users.noreply.github.com>
2024-10-21 22:46:08 +1300

    refactor SecretClickSounds

--- src/main/kotlin/com/ownwn/ownwnaddons/feature/dungeons/SecretClickSounds.kt
+++ src/main/kotlin/com/ownwn/ownwnaddons/feature/dungeons/SecretClickSounds.kt
@@ -1,20 +1,18 @@
-package com.ownwn.ownwnaddons.features.dungeons
+package com.ownwn.ownwnaddons.feature.dungeons
 
-import com.ownwn.ownwnaddons.OwnwnAddons
-import com.ownwn.ownwnaddons.utils.NewConfig
+import com.ownwn.ownwnaddons.Config
+import com.ownwn.ownwnaddons.util.Utils
 import net.minecraft.client.Minecraft
 import net.minecraft.init.Blocks
 import net.minecraft.tileentity.TileEntitySkull
 import net.minecraftforge.event.entity.player.PlayerInteractEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
-class SecretClickSounds {
+object SecretClickSounds {
     private var lastSound: Long = 0
-    private val witherEssenceUUID = "26bb1a8d-7c66-31c6-82d5-a9c04c94fb02"
+    private const val WITHER_ESSENCE_UUID = "26bb1a8d-7c66-31c6-82d5-a9c04c94fb02"
 
     private val blockSet = setOf(Blocks.chest, Blocks.trapped_chest, Blocks.skull, Blocks.lever)
-
-
 
 
     @SubscribeEvent
@@ -22,8 +20,8 @@ class SecretClickSounds {
 
         if (event.action != PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) return
 
-        if (NewConfig.SECRET_CLICK_SOUND.isEmpty()) return
-        if (!OwnwnAddons.utils.checkLocGameMode("dungeon")) return
+        if (Config.secretClickSound.isEmpty()) return
+        if (!Utils.isInGameMode("dungeon")) return
 
         val world = Minecraft.getMinecraft().theWorld
         val theBlock = world?.getBlockState(event.pos)?.block ?: return
@@ -34,7 +32,7 @@ class SecretClickSounds {
             val tileEntity = world.getTileEntity(event.pos) as? TileEntitySkull ?: return
             val skullId = tileEntity.playerProfile?.id ?: return
 
-            if (skullId.toString() != witherEssenceUUID) return
+            if (skullId.toString() != WITHER_ESSENCE_UUID) return
         }
 
         playSound()
@@ -46,12 +44,10 @@ class SecretClickSounds {
         if (System.currentTimeMillis() - lastSound < 50) return // don't kill ears
 
         Minecraft.getMinecraft().thePlayer.playSound(
-            NewConfig.SECRET_CLICK_SOUND,
-            NewConfig.SECRET_CLICK_VOLUME / 10,
-            NewConfig.SECRET_CLICK_PITCH / 10
+            Config.secretClickSound,
+            Config.secretClickVolume / 10,
+            Config.secretClickPitch / 10
         )
         lastSound = System.currentTimeMillis()
     }
-
-
 }
