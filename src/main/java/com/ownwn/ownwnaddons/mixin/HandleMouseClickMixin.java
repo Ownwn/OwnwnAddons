579f8eaa04b5ff5435e3ecff7d81c8527c1b9a30
Ownwn <64725743+Ownwn@users.noreply.github.com>
2024-10-21 22:44:38 +1300

    refactor block necromancy soul deletion

--- src/main/java/com/ownwn/ownwnaddons/mixin/HandleMouseClickMixin.java
+++ src/main/java/com/ownwn/ownwnaddons/mixin/HandleMouseClickMixin.java
@@ -1,10 +1,10 @@
 package com.ownwn.ownwnaddons.mixin;
 
 import cc.polyfrost.oneconfig.libs.universal.UChat;
+import com.ownwn.ownwnaddons.Config;
 import com.ownwn.ownwnaddons.OwnwnAddons;
-import com.ownwn.ownwnaddons.utils.CheckSlot;
-import com.ownwn.ownwnaddons.utils.NewConfig;
-import net.minecraft.client.Minecraft;
+import com.ownwn.ownwnaddons.util.CheckSlot;
+import com.ownwn.ownwnaddons.util.Game;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,13 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiContainer.class)
 public class HandleMouseClickMixin {
 
-    private static final String[] DUNGEON_ITEMS = {"Kismet", "Spirit Leap", "Superboom TNT"};
-    // this should probably be user-adjustable, but it works for now
-
     @Inject(method = "handleMouseClick", at = @At(value = "HEAD"), cancellable = true)
     public void handleMouseClick(Slot slotIn, int slotId, int mouseButtonClicked, int clickType, CallbackInfo ci) {
 
-        if (Minecraft.getMinecraft().thePlayer == null) {
+        if (Game.INSTANCE.getPlayer() == null) {
             return;
         }
         if (slotIn == null || slotIn.getStack() == null) {
@@ -30,21 +27,12 @@ public class HandleMouseClickMixin {
         }
 
 
-        if (NewConfig.STOP_INSTA_SELL) {
-            if (CheckSlot.checkSlotNameAndLore(slotIn, "Sell Inventory Now", DUNGEON_ITEMS)) {
-                ci.cancel();
-                UChat.chat(OwnwnAddons.PREFIX + "&aStopped you from insta-selling your dungeon items!" +
-                        "\n &aDisable this in &b/owa");
-                return;
-            }
-        }
-
-        if (NewConfig.STOP_NECROMANCER_REMOVE) {
-            if (!CheckSlot.openGuiName.equals("Soul Menu")) {
+        if (Config.INSTANCE.getStopNecromancerDelete()) {
+            if (!CheckSlot.INSTANCE.getOpenGuiName().equals("Soul Menu")) {
                 return;
             }
 
-            if (CheckSlot.checkSlotNameAndLore(slotIn, "", new String[]{"Base Mana Cost"})) { // leave name blank because the mob souls have different item names
+            if (CheckSlot.INSTANCE.checkSlotNameAndLore(slotIn, "", new String[]{"Base Mana Cost"})) { // leave name blank because the mob souls have different item names
                 ci.cancel();
                 UChat.chat(OwnwnAddons.PREFIX + "&aStopped you from deleting that soul!" +
                         "\n &aDisable this in &b/owa");
---
+++ src/main/kotlin/com/ownwn/ownwnaddons/util/CheckSlot.kt
@@ -0,0 +1,53 @@
+package com.ownwn.ownwnaddons.util
+
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.inventory.GuiChest
+import net.minecraft.inventory.ContainerChest
+import net.minecraft.inventory.Slot
+import net.minecraft.item.ItemStack
+import net.minecraftforge.client.event.GuiOpenEvent
+import net.minecraftforge.fml.common.eventhandler.EventPriority
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+
+object CheckSlot {
+    var openGuiName: String = "" // the name of the currently open chest gui
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    fun onOpenGui(event: GuiOpenEvent) { // https://github.com/Moulberry/NotEnoughUpdates/blob/master/src/main/java/io/github/moulberry/notenoughupdates/util/SBInfo.java
+        if (event.gui !is GuiChest) {
+            openGuiName = ""
+            return
+        }
+
+        val chest = event.gui as GuiChest
+        val chestContainer = chest.inventorySlots as ContainerChest
+
+        openGuiName = Utils.stripFormatting(chestContainer.lowerChestInventory.displayName.unformattedText)
+    }
+
+    fun checkSlotNameAndLore(slot: Slot, name: String, lore: Array<String>): Boolean {
+        // hasDisplayName() returns true if the item has an italic name (has been renamed in an anvil)
+        // or has been renamed with a plugin/command (like in sb), but getDisplayName() correctly grabs the sb name.
+
+
+        val slotStack: ItemStack = slot.stack
+
+        if (!Utils.stripFormatting(slotStack.displayName).contains(name)) {
+            return false
+        }
+
+        val loreList = slotStack.getTooltip(Minecraft.getMinecraft().thePlayer, false)
+
+        for (itemLore in loreList) { // loop through the lines of lore on the item
+
+            for (loreMatch in lore) { // loop through the String[] array of item names
+                if (Utils.stripFormatting(itemLore).contains(loreMatch)) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+}
